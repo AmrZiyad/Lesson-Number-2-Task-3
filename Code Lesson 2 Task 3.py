0fffb9cf742b45ee8f7baf09a2b2aa7f2f76fd1c
zxyd_amr <mail.amrziyad@gmail.com>
2026-05-13 19:24:42 +0300

    Add files via upload

---
+++ Code Lesson 2 Task 3.py
@@ -0,0 +1,20 @@
+#introduction about amr
+print("Hello, My Name Is Amr")
+
+age=13
+#his age
+print("I am ",age,"years old")
+
+subject = "python"
+
+#learning about his favorite subject
+print("I am learning ",subject,)
+
+#hobby which he likes to do
+
+play= "football"
+print("I like ",play,)
+
+import keyword
+print("Python keywords are:")
+print(keyword.kwlist)
